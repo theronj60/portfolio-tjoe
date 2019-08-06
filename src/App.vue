0a370319476900d18466681652c44072d35b3b4d
theron.joe <theronj@joetwebdev.io>
2019-08-05 23:36:44 -0600

    configured new router, add new components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<div id="app">
+    <Navbar />
+    <router-view />
+    <Footer />
+</div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
+export default {
+    name: 'app',
+    components: {
+        Navbar,
+        Footer
+    }
+}
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <h5>Footer here</h5>
+
+</template>
+
+<script>
+export default {
+}
+</script>
